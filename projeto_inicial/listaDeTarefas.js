85278bdeb4c0c8989f44808e395d69d6613aa5a4
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-10-14 04:09:44 -0300

    Add button concluir

--- projeto_inicial/listaDeTarefas.js
+++ projeto_inicial/listaDeTarefas.js
@@ -2,21 +2,36 @@ const criarTarefa = (evento) => {
 
     evento.preventDefault()
 
-    const lista = document.querySelector('[data-list')
-    const input = document.querySelector('[data-form-input')
-    const valor = input.value
+    const lista = document.querySelector('[data-list]');
+    const input = document.querySelector('[data-form-input]');
+    const valor = input.value;
     
-    const tarefa = document.createElement('li')
-    tarefa.classList.add('task')
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
     
-    const conteudo = `<p class="content">${valor}</p>`
+    const conteudo = `<p class="content">${valor}</p>`;
 
-    tarefa.innerHTML = conteudo
+    tarefa.innerHTML = conteudo;
 
-    lista.appendChild(tarefa)
-    input.value = " "
+    tarefa.appendChild(BotaoConclui());
+    lista.appendChild(tarefa);
+    input.value = " ";
 }
 
-const novaTarefa = document.querySelector('[data-form-button]')
+const novaTarefa = document.querySelector('[data-form-button]');
 
-novaTarefa.addEventListener("click", criarTarefa)
+novaTarefa.addEventListener("click", criarTarefa);
+
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button');
+
+    botaoConclui.classList.add('check-button');
+    botaoConclui.innerText = 'concluir';
+
+    botaoConclui.addEventListener('click', () => {
+        console.log('fui clicado');
+
+    })
+
+    return botaoConclui;
+}
